a61f9a29ccbb4e74dd42bf9d08af6fa772b0c3db
Greg Carbue <greg@carnie.ca>
2016-04-18 07:04:59 -0600

    Updated DSC module version.

--- SharePoint ARM projects/Scripts/Deploy-AzureResourceGroup.ps1
+++ SharePoint ARM projects/Scripts/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,8 @@
 
 Param(
   [string] [Parameter(Mandatory=$false)] $ResourceGroupLocation = 'eastus',
-  [string] [ValidateSet('SP2013-SingleServer','SP2013-SmallFarm','SP2016-SingleServer','SP2016-SmallFarm')] [Parameter(Mandatory=$false)] $ResourceGroupName = 'SP2016-SmallFarm',
+  [string] [ValidateSet('SP2013-SingleServer','SP2013-SmallFarm','SP2016-SingleServer','SP2016-SmallFarm')] [Parameter(Mandatory=$false)] $ConfigName = 'SP2016-SmallFarm',
+  [string] [Parameter(Mandatory=$false)] $ResourceGroupName = 'SharePoint2016-Test02',
   [switch] $SharePointOnly
 )
 
@@ -18,31 +19,32 @@ Write-Output -InputObject "**********************************************"
 Write-Output -InputObject "Begining Creation of SharePoint ARM Deployment"
 Write-Output -InputObject "**********************************************"
 Write-Output -InputObject "Resource Group: $ResourceGroupName"
-Write-Output -InputObject "Region:         $ResourceGroupLocation"
+Write-Output -InputObject "Region: $ResourceGroupLocation"
+Write-Output -InputObject "Configuration: $ConfigName"
 Write-Output -InputObject " "
 
 #Login-AzureRmAccount
 
 
-$storageSettings = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\storage.parameters.json" -Resolve) -Raw | ConvertFrom-Json
+$storageSettings = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\storage.parameters.json" -Resolve) -Raw | ConvertFrom-Json
 
 if (-not $SharePointOnly) {
     Write-Output -InputObject "Task 1: Creating storage account"
     $ErrorActionPreference = "Stop"
     New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
     New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
-                           -TemplateFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\Storage.json" -Resolve) `
-                           -TemplateParameterFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\Storage.parameters.json" -Resolve) `
+                           -TemplateFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\Storage.json" -Resolve) `
+                           -TemplateParameterFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\Storage.parameters.json" -Resolve) `
                            -Verbose
 
     Start-Sleep -Seconds 60
 
     Write-Output -InputObject "Task 2: Starting copy of VM image for SharePoint Servers"
-    $vmimageSettings = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\vmimage.json" -Resolve) -Raw | ConvertFrom-Json
+    $vmimageSettings = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\vmimage.json" -Resolve) -Raw | ConvertFrom-Json
     $imageCopyJob = Copy-AzureVMImageToStorageContainer -SourceStorageAccountName $vmimageSettings.AccountName -SourceStorageAccountKey $vmimageSettings.AccountKey -ImageUrl $vmimageSettings.Uri -TargetStorageAccountName $storageSettings.parameters.primaryStorageAccountName.value -ResourceGroupName $ResourceGroupName
 
     Write-Output -InputObject "Task 3: Publishing DSC configurations"
-    $ConfigurationPath = Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\Configurations" -Resolve
+    $ConfigurationPath = Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\Configurations" -Resolve
     Get-ChildItem -Path $ConfigurationPath -File -Filter "*.ps1" | ForEach-Object {
         Publish-AzureRmVMDscConfiguration -ConfigurationPath $_.FullName -ResourceGroupName $ResourceGroupName -StorageAccountName $storageSettings.parameters.dscStorageAccountName.value -SkipDependencyDetection -Force
     }
@@ -51,8 +53,8 @@ if (-not $SharePointOnly) {
 
     New-AzureRMResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
                                      -Name "CommonInfrastructure" `
-                                     -TemplateFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\InfrastructureServers.json" -Resolve) `
-                                     -TemplateParameterFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\InfrastructureServers.parameters.json" -Resolve) `
+                                     -TemplateFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\InfrastructureServers.json" -Resolve) `
+                                     -TemplateParameterFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\InfrastructureServers.parameters.json" -Resolve) `
                                      -dscSasToken (New-DscConfigurationsSasToken -ResourceGroupName $ResourceGroupName -StorageAccountName $storageSettings.parameters.dscStorageAccountName.value) `
                                      -StorageAccountName $storageSettings.parameters.primaryStorageAccountName.value `
                                      -dscStorageAccountName $storageSettings.parameters.dscStorageAccountName.value `
@@ -64,7 +66,7 @@ if (-not $SharePointOnly) {
     Write-Output -InputObject "Task 6: Provisioning SharePoint Servers"
 } else {
     Write-Output -InputObject "Task 1: Publishing DSC configurations"
-    $ConfigurationPath = Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\Configurations" -Resolve
+    $ConfigurationPath = Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\Configurations" -Resolve
     Get-ChildItem -Path $ConfigurationPath -File -Filter "*.ps1" | ForEach-Object {
         Publish-AzureVMDscConfiguration -ResourceGroupName $ResourceGroupName -ConfigurationPath $_.FullName -StorageAccountName $storageSettings.parameters.dscStorageAccountName.value -Force | Out-Null 
     }
@@ -74,8 +76,8 @@ if (-not $SharePointOnly) {
 
 New-AzureRMResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
                                  -Name "SharePointServers" `
-                                 -TemplateFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\SharePointServers.json" -Resolve) `
-                                 -TemplateParameterFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ResourceGroupName\SharePointServers.parameters.json" -Resolve) `
+                                 -TemplateFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\SharePointServers.json" -Resolve) `
+                                 -TemplateParameterFile (Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$ConfigName\SharePointServers.parameters.json" -Resolve) `
                                  -dscSasToken (New-DscConfigurationsSasToken -ResourceGroupName $ResourceGroupName -StorageAccountName $storageSettings.parameters.dscStorageAccountName.value) `
                                  -StorageAccountName $storageSettings.parameters.primaryStorageAccountName.value `
                                  -dscStorageAccountName $storageSettings.parameters.dscStorageAccountName.value `

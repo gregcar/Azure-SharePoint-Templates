b42e609215d3352089861fcd8ac5efb876d1e3e5
Greg Carbue <greg@carnie.ca>
2016-04-21 22:51:33 -0600

    Added dependancy for domain to be ready before sql config.

--- SharePoint-2016-HA-Farm/Scripts/Deploy-AzureResourceGroup.ps1
+++ SharePoint-2016-HA-Farm/Scripts/Deploy-AzureResourceGroup.ps1
@@ -3,8 +3,8 @@
 #Requires -Module Azure.Storage
 
 Param(
-    [string] $ResourceGroupLocation = 'eastus2',
-    [string] $ResourceGroupName = 'SharePoint2016-Test02',
+    [string] $ResourceGroupLocation = 'eastus',
+    [string] $ResourceGroupName = 'SharePoint2016-Test03',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName = 'gc-sp2016test01',
 	[string] $dnsPrefix = 'gcspt01',
